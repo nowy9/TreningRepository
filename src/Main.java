be577dec195d56a5a252636d2e2be0305d407cf3
Adrian Żak <buty.zaklin@gmail.com>
2019-12-15 14:01:51 +0100

    Krótki opis zmian w commicie

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,24 @@
+package pl.kul;
+
+// import pl.kul.printer.MoviesPrinter;
+// import pl.kul.repository.TsvFileBasedMoviesRepository;
+// import pl.kul.repository.MoviesRepository;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Main {
-    //...
+    public static void main(String[] args) {
+        // Utworzenie repozytorium filmów
+       // Path inputFilePath = Paths.get(Main.class.getResource("/title.akas.tsv.gz").getFile());
+       // MoviesRepository moviesRepository = new TsvFileBasedMoviesRepository(inputFilePath);
+
+        // Wyciągnięcie filmów z repozytorium
+      //  List<Movie> allMovies = moviesRepository.findAll();
+
+        //Wyświetlenie tego co w zadaniu
+       // MoviesPrinter moviesPrinter = new MoviesPrinter(System.out);
+        // moviesPrinter.print(allMovies);
+    }
 }
